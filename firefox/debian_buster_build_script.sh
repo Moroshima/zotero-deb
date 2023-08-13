c9058573791ac87acd3af7a29c3d6ad6b61c088b
Moroshima <moroshimaitsuki@gmail.com>
2023-08-14 04:44:57 +0800

    add firefox build script on debian buster

---
+++ firefox/debian_buster_build_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#apt install mercurial
+#hg clone --config ui.clonebundles=false https://hg.mozilla.org/releases/mozilla-esr60/
+
+wget https://ftp.mozilla.org/pub/firefox/releases/60.9.0esr/source/firefox-60.9.0esr.source.tar.xz
+tar -xvf firefox-60.9.0esr.source.tar.xz
+
+apt install llvm clang
+
+cd firefox-60.9.0
+
+./mach bootstrap
+
+rustup install 1.32.0
+rustup default 1.32.0
+
+./mach build
+#./mach run
